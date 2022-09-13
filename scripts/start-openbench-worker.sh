65b37be9aef1ea5fac1d929129e9d372feb79fdd
Sami Kiminki <skiminki@users.noreply.github.com>
2022-09-13 12:36:43 +0300

    Add support for outside cache
    The outside cache can be used to store persistent data such as engine
    builds, networks, and rust/cargo temporary files. This avoids
    unnecessary rebuilds and refetches when the container is restarted.

--- scripts/start-openbench-worker.sh
+++ scripts/start-openbench-worker.sh
@@ -2,23 +2,22 @@
 set -e
 cd "$(dirname $0)"
 PWD="$(pwd)"
-IMAGE=skiminki/openbench-client
+IMAGE=skiminki/openbench-client:latest
 CONFIG=config.sh
 
 if [ -f "${PWD}/${CONFIG}" ]
 then
 	source "${PWD}/${CONFIG}"
+	args=("--mount" "type=bind,source=${PWD}/${CONFIG},target=/${CONFIG},readonly")
 	if [ -n "${SYZYGY}" ]
 	then
-		docker run -it --rm --name openbench-client \
-			--mount type=bind,source="${PWD}/${CONFIG}",target="/${CONFIG}",readonly \
-			--mount type=bind,source="${SYZYGY}/",target="/syzygy",readonly \
-			"${IMAGE}" "$@"
-	else
-		docker run -it --rm --name openbench-client \
-			--mount type=bind,source="${PWD}/${CONFIG}",target="/${CONFIG}",readonly \
-			"${IMAGE}" "$@"
+		args+=("--mount" "type=bind,source=${SYZYGY},target=/syzygy/,readonly")
 	fi
+	if [ -n "${CACHE}" ]
+	then
+		args+=("--mount" "type=bind,source=${CACHE},target=/cache/")
+	fi
+	docker run -it --rm --name openbench-client ${args[@]} "${IMAGE}" "$@"
 else
 	docker run -it --rm --name openbench-client "${IMAGE}"
 fi
