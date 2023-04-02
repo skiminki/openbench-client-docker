1d6136981f775b347488a5bffff740ac4f1cbd0a
Sami Kiminki <skiminki@users.noreply.github.com>
2023-04-02 12:12:07 +0300

    Use the first argument to build.sh as the docker image tag when provided..
    ... and when not, then use the default tag 'latest'.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e
 OPENBENCH_GIT_HASH="$(git ls-remote https://github.com/skiminki/OpenBench.git HEAD | head -n 1 | awk '{ print $1 }')"
 
 docker build . \
-       -t skiminki/openbench-client:latest \
+       -t skiminki/openbench-client:${1:-latest} \
        --build-arg "OPENBENCH_GIT_HASH=${OPENBENCH_GIT_HASH}"
